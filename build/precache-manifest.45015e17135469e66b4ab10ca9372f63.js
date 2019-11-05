2e654d8aa00bc705a307f2a442d74aace066db37
djdouta <44418010+djdouta@users.noreply.github.com>
2019-11-05 12:28:25 -0300

    Add files via upload

---
+++ build/precache-manifest.45015e17135469e66b4ab10ca9372f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46b32164ea283e818be0e682234558d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2502aaa09fe139ed9930",
+    "url": "/static/css/main.a780ce44.chunk.css"
+  },
+  {
+    "revision": "0db92736304d9204b9aa",
+    "url": "/static/js/2.b23008aa.chunk.js"
+  },
+  {
+    "revision": "2502aaa09fe139ed9930",
+    "url": "/static/js/main.60bca04e.chunk.js"
+  },
+  {
+    "revision": "2fed07cfcdc30d5d00dc",
+    "url": "/static/js/runtime-main.3837595e.js"
+  }
+]);
